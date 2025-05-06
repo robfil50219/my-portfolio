a7b062f60eb77712876e7d3443be02006f26e24b
robfil50219 <robfil50219@stud.noroff.no>
2025-05-06 14:00:53 +0200

    get in touch section updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,7 +5,7 @@ export default function Contact() {
         <section id="contact" className="bg-gray-100 py-20">
             <div className="container mx-auto px-6 text-center">
                 <h2 className="text-3xl font-bold mb-4">Get In Touch</h2>
-                <p className="mb-8">Feel free to reach out for collaborations or a chat.</p>
+                <p className="mb-8">Feel free to reach out. I’m always open to new projects, collaborations, or just a friendly chat about the latest in front-end development.</p>
 
                 <div className="flex justify-center space-x-4">
                     <a
