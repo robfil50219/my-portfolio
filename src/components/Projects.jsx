90c9a416cf6908a2836e047a01d1c84b5ddfbd74
robfil50219 <robfil50219@stud.noroff.no>
2025-06-03 13:55:25 +0200

    added tadoo project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,10 +4,10 @@ import ProjectCard from './ProjectCard';
 
 const projects = [
     {
-        title: 'Auctionary',
-        description: 'A real-time auction platform built with React and WebSockets. Users can create items, place live bids, and watch prices update instantly.',
-        image: '/images/Auctionary.png',
-        link: 'https://auctionarygo.netlify.app/',
+        title: 'Tadoo',
+        description: 'Tadoo is a simple family To-Do app built with Angular that stores all tasks locally in the browser’s localStorage. You can add, edit, and check off tasks without any external database. This was my very first Angular project, and it helped me learn the core concepts—components, services, and two-way data binding with ngModel—while structuring a basic application from scratch. It’s still a work in progress, and I’ll be adding more changes and features as I continue learning.',
+        image: 'images/Tadoo.png',
+        link: 'https://tadoo.netlify.app/',
     },
     {
         title: 'Holidaze',
